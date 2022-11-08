37ae8376abd674056ef82643ba58595be143742d
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-09 02:22:56 +0600

    sorted review data

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
     app.get('/reviews/:id', async (req, res) => {
         const id = req.params.id;
         const query = { service: id };
-        const cursor = reviewCollection.find(query);
+        const cursor = reviewCollection.find(query).sort({ insertionTime: -1 });
         const reviews = await cursor.toArray();
         res.send(reviews)
     })
