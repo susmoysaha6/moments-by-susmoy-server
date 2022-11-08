c07c9542d9a7091e322b5a008c82e4a5265c3f9f
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-08 21:04:25 +0600

    /services/:id api create for dynamic single service

--- index.js
+++ index.js
@@ -23,13 +23,20 @@ async function run() {
         const cursor = serviceCollection.find(query);
         const services = await cursor.toArray();
         res.send(services)
-    })
+    });
 
     app.get('/services-on-homepage', async (req, res) => {
         const query = {};
         const cursor = serviceCollection.find(query);
         const services = await cursor.limit(3).toArray();
         res.send(services)
+    });
+
+    app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
     })
 
 }
