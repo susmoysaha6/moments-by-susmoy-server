df0cd2901784f96a0461dc76bacb7eba1ec65bbf
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-08 15:41:42 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xuxjswq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 app.get('/', (req, res) => {
     res.send('photography server is running')
 })
