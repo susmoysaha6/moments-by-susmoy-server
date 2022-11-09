a77293928a968360b2c10ec7d77588ac18014f19
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-09 23:03:31 +0600

    jwt implemented for all routes using in my service pages

--- index.js
+++ index.js
@@ -15,11 +15,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xuxjswq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// JWT middleware
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('photographyServices').collection('services');
         const reviewCollection = client.db('photographyServices').collection('reviews');
+        // JWT
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
         // services api
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query).sort({ insertionTime: -1 });
@@ -47,7 +70,12 @@ async function run() {
         })
         // review api
 
-        app.get('/reviews', async (req, res) => {
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
+
             let query = {};
             if (req.query.email) {
                 query = {
@@ -59,12 +87,13 @@ async function run() {
             res.send(reviews)
         });
 
-        app.post('/reviews', async (req, res) => {
+        app.post('/reviews', verifyJWT, async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         });
 
+        // api for display review on service details page
         app.get('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { service: id };
@@ -73,14 +102,14 @@ async function run() {
             res.send(reviews)
         });
 
-        app.get('/review/:id', async (req, res) => {
+        app.get('/review/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const review = await reviewCollection.findOne(query);
             res.send(review);
         });
 
-        app.patch('/review/:id', async (req, res) => {
+        app.patch('/review/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await reviewCollection.updateOne(query, {
@@ -90,7 +119,7 @@ async function run() {
             res.send(result);
         });
 
-        app.delete('/reviews/:id', async (req, res) => {
+        app.delete('/reviews/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
 
